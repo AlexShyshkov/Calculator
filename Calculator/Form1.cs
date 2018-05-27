7e2386a255d705ab389196d1ede3118444b565cc
Alex Shyshkov <shi-alex@ukr.net>
2018-05-27 23:03:40 +0300

    1

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -1,20 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class CalculatorForm : Form
     {
-        public Form1()
+        int value ;
+        string operation = "";
+        bool isOperationSignPressed = false;
+            
+        public CalculatorForm()
         {
             InitializeComponent();
+        }
+
+            private void Calculate()
+        {
+            preview.Text = "";
+            switch (operation)
+            {
+                case "+":
+                    calculatorTextBox.Text = (value+ int.Parse(calculatorTextBox.Text)).ToString();
+                    break;
+                case "-":
+                    calculatorTextBox.Text = (value - int.Parse(calculatorTextBox.Text)).ToString();
+                    break;
+                case "*":
+                    calculatorTextBox.Text = (value * int.Parse(calculatorTextBox.Text)).ToString();
+                    break;
+                case "/":
+                    if (calculatorTextBox.Text == "0")
+                    {
+                        MessageBox.Show($"Cannot divide by zero");
+                    }
+                    else
+                    {
+                        calculatorTextBox.Text = (value / int.Parse(calculatorTextBox.Text)).ToString();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((calculatorTextBox.Text == "0")||(isOperationSignPressed))
+            {
+                calculatorTextBox.Clear();
+            }
+            isOperationSignPressed = false;
+            Button button = (Button)sender;
+            calculatorTextBox.Text = calculatorTextBox.Text + button.Text;
+
+        }
+
+        private void buttonOperation_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Text;
+            value = int.Parse(calculatorTextBox.Text);
+            isOperationSignPressed = true;
+            preview.Text = value + " " + operation;
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            Calculate();
+            preview.Text = "";
+        }
+        
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            if (calculatorTextBox.Text == null)
+            {
+                preview.Text = "";
+            }
+            else if(calculatorTextBox.Text != null)
+            {
+                calculatorTextBox.Clear();
+                preview.Text = "";
+            }
         }
     }
 }
